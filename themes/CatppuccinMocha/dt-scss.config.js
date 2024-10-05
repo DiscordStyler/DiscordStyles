c86bb08c51540ec47cab6b65971c920b473dae9f
0xJWLabs <0xJWLabs@gmail.com>
2024-10-05 08:04:39 +0700

    style: fix style

--- themes/CatppuccinMocha/dt-scss.config.js
+++ themes/CatppuccinMocha/dt-scss.config.js
@@ -1,25 +1,21 @@
 export default {
-	meta: {
-		name: 'Catppuccin Mocha',
-		author: 'jw0902',
-		version: '0.1.0',
-		description: '🎮 Soothing pastel theme for Discord',
-		source: 'https://github.com/DiscordStyler/DiscordStyles',
-	},
-	base: {
-		target: "themes/CatppuccinMocha/base.scss",
-		output: 'dist/CatppuccinMocha'
-	},
-    dist: {
-        target: "themes/CatppuccinMocha/dist.scss",
-        output: "dist/CatppuccinMocha",
-    },
-	// dev: {
-    //     target: "themes/CatppuccinMocha/dev.scss",
-    //     output: "dist/CatppuccinMocha",
-    // },
-	baseImport: 'https://discordstyler.github.io/DiscordStyles/CatppuccinMocha/CatppuccinMocha.css',
-	addons: [
-		['themes/CatppuccinMocha/addons/_unrounding.scss', 'dist/CatppuccinMocha/Unrounding.css'],
-	]
+  meta: {
+    name: 'Catppuccin Mocha',
+    author: 'jw0902',
+    version: '0.1.0',
+    description: '🎮 Soothing pastel theme for Discord',
+    source: 'https://github.com/DiscordStyler/DiscordStyles',
+  },
+  base: {
+    target: "themes/CatppuccinMocha/base.scss",
+    output: 'dist/CatppuccinMocha'
+  },
+  dist: {
+    target: "themes/CatppuccinMocha/dist.scss",
+    output: "dist/CatppuccinMocha",
+  },
+  baseImport: 'https://discordstyler.github.io/DiscordStyles/CatppuccinMocha/CatppuccinMocha.css',
+  addons: [
+    ['themes/CatppuccinMocha/addons/_unrounding.scss', 'dist/CatppuccinMocha/Unrounding.css'],
+  ]
 };
--- themes/Ultra/dt-scss.config.js
+++ themes/Ultra/dt-scss.config.js
@@ -1,28 +1,24 @@
 export default {
-	meta: {
-		name: 'Ultra',
-		author: 'jw0902',
-		version: '1.0.0',
-		description: 'My cool theme',
-		source: 'https://github.com/DiscordStyler/DiscordStyles',
-	},
-	base: {
-		target: "themes/Ultra/base.scss",
-		output: 'dist/Ultra'
-	},
-    dist: {
-        target: "themes/Ultra/dist.scss",
-        output: "dist/Ultra",
-    },
-	// dev: {
-    //     target: "themes/Ultra/dev.scss",
-    //     output: "dist/Ultra",
-    // },
-	baseImport: 'https://discordstyler.github.io/DiscordStyles/Ultra/Ultra.css',
-    addons: [
-		['themes/Ultra/addons/_banners.scss', 'dist/Ultra/Banners.css'],
-		['themes/Ultra/addons/_betterInvites.scss', 'dist/Ultra/BetterInvites.css'],
-		['themes/Ultra/addons/_touchup.scss', 'dist/Ultra/TouchUp.css'],
-        ['themes/Ultra/addons/_userBadges.scss', 'dist/Ultra/UserBadges.css'],
-	]
+  meta: {
+    name: 'Ultra',
+    author: 'jw0902',
+    version: '1.0.0',
+    description: 'My cool theme',
+    source: 'https://github.com/DiscordStyler/DiscordStyles',
+  },
+  base: {
+    target: "themes/Ultra/base.scss",
+    output: 'dist/Ultra'
+  },
+  dist: {
+    target: "themes/Ultra/dist.scss",
+    output: "dist/Ultra",
+  },
+  baseImport: 'https://discordstyler.github.io/DiscordStyles/Ultra/Ultra.css',
+  addons: [
+    ['themes/Ultra/addons/_banners.scss', 'dist/Ultra/Banners.css'],
+    ['themes/Ultra/addons/_betterInvites.scss', 'dist/Ultra/BetterInvites.css'],
+    ['themes/Ultra/addons/_touchup.scss', 'dist/Ultra/TouchUp.css'],
+    ['themes/Ultra/addons/_userBadges.scss', 'dist/Ultra/UserBadges.css'],
+  ]
 };
